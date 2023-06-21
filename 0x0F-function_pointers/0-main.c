3f1e57b6703b3e5a7004b0fb1ce4152a2afdd288
meryemouirem <meryemouirem94@gmail.com>
2023-06-21 04:44:49 -0700

    0.main

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -11,6 +11,7 @@ void print_name_as_is(char *name)
 {
 	printf("Hello, my name is %s\n", name);
 }
+
 /**
  * print_name_uppercase - print a name in uppercase
  * @name: name of the person
@@ -20,22 +21,23 @@ void print_name_as_is(char *name)
 void print_name_uppercase(char *name)
 {
 	unsigned int i;
-	
+
 	printf("Hello, my uppercase name is ");
 	i = 0;
 	while (name[i])
 	{
 		if (name[i] >= 'a' && name[i] <= 'z')
-        {
-		putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-		putchar(name[i]);
-        }
-	i++;
-    }
+		{
+			putchar(name[i] + 'A' - 'a');
+		}
+		else
+		{
+			putchar(name[i]);
+		}
+		i++;
+	}
 }
+
 /**
  * main - check the code
  *
